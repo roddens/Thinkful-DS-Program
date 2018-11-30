6936942ab2c7ca7556a78dac092f1434d2d6c4f6
Andrew Zirm <andrewzirm@gmail.com>
2018-11-29 20:55:21 -0500

    Updated function defs and added new file for today

---
+++ mentor-sessions/20181125.py
@@ -0,0 +1,9 @@
+def hello(person):
+    output_string = 'Hello, ' + person
+    print(output_string)
+    return output_string
+
+def quotable(name, quote):
+    output_string = name + ' said: ' + '"' + quote + '"'
+    print(output_string)
+    return output_string
